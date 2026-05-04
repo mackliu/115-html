01982a9698408a42522bec93e9d58a15ad0db816
mackliu <macklun@gmail.com>
2026-05-04 11:42:22 +0800

    date()參數練習

--- 02-datetime.php
+++ 02-datetime.php
@@ -50,6 +50,35 @@
     
     ?>
 
+<h2>利用date()函式的格式化參數，完成以下的日期格式呈現</h2>
+
+<ul>
+    <li>2021/10/05</li>
+    <li>10月5日 Tuesday</li>
+    <li>2021-10-5 12:9:5</li>
+    <li>2021-10-5 12:09:05</li>
+    <li>今天是西元2021年10月5日 上班日(或假日)</li>
+</ul>
+<?php 
+
+echo date("Y/m/d");
+echo "<br>";
+echo date("n月j日 l");
+echo "<br>";
+echo date("Y-m-d G:").(int)date("i").":".(int)date("s");
+echo "<br>";
+echo date("Y-m-d H:i:s");
+echo "<br>";
+echo "今天是西元";
+echo  date("Y年m月d日");
+echo  (date("N")>5)?" 假日":" 上班日";
+echo "<br>";
+
+
+
+
+?>
+
     <p>&nbsp;</p>
     <p>&nbsp;</p>
     <p>&nbsp;</p>
